a953e096fa7758a12d41ee507352567427b71a23
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-04-11 19:44:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-/*some code would go here*/
+"use strict";
+var f_select = document.getElementById('f')
+var result = document.getElementById('result')
+f_select.onchange = function(e){
+console.log(e)
+  result.textContent = "Scanning..."
+  window.setTimeout(function(){
+    result.innerText = "Result: This is a file\nDetection: File.MachineLearning100\nConfidence: 100%"
+  },20)
+}
